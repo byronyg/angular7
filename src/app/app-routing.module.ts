fe0f87009035c2a71908dce6d0683aa9bf4cafe6
Yego's MacBook Pro <mac@macs-MacBook-Pro.local>
2018-11-15 16:17:39 +0300

    Fist commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/**
+******************* my first mistake ***********************
+import { Homecomponent }, from './home/home.component';
+import { Aboutcomponent }, from './about/about.component';
+import { Contactcomponent }, from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Homecomponent },
+  { path: 'about', component: Aboutcomponent },
+  { path: 'contact', component: Concomponent },
+];
+*/
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
